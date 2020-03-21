35ddeead6b83a4a576888e303b4b9c2e19fdaca1
Alice <alice@Alices-MacBook-Air.local>
2020-03-21 16:24:14 -0700

    Remove dummy expense data

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ import getVisibleExpenses from './selectors/expenses'
 
 const store = configureStore();
 
-store.dispatch(addExpense({description:'Water Bill', amount: 4500}));
-store.dispatch(addExpense({description:'Gas Bill', createdAt:1000, amount: 20000}));
-store.dispatch(addExpense({description:'Rent', amount: 109500}));
+// store.dispatch(addExpense({description:'Water Bill', amount: 4500}));
+// store.dispatch(addExpense({description:'Gas Bill', createdAt:1000, amount: 20000}));
+// store.dispatch(addExpense({description:'Rent', amount: 109500}));
 
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);  
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);  
+// console.log(visibleExpenses);
 
 class App extends React.Component{
 
